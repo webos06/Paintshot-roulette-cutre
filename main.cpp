049db13815a201eb2036e9ea0b32270be83ebd86
funes451 <david.funes@stnicolau.com>
2026-01-14 11:36:59 +0100

    Nombre de funciones

--- main.cpp
+++ main.cpp
@@ -1,40 +1,56 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <ctime>   
+#include <cstdlib>  
+#include <fstream>
+#include <algorithm>
 using namespace std;
 
-void mostrar_menu();
-void procesar_opcio_menu(int opcio);
-void triar_opcio_menu();
+struct EntradaRanking {
+    string nombre;
+    int puntuacion;
+};
 
-int main(){
-    triar_opcio_menu();
+struct Jugador {
+    string nombre;
+    int vidas;
+    vector<int> objetos;
+};
+
+//Funcion principal
+int jugar_juego();
+
+int NumRandom(int minimo, int maximo);
+int enterusuario();
+
+// Ranking
+bool Puntuacion(string nombre, int monedas);
+vector<EntradaRanking> Rankingtexto();
+int listaranking();
+
+// Logica del juego
+int municion_total(int fasejuego, int recargaescopeta);
+int municion_cargador(vector <int> & recamara, int fasejuego, bool dobleonada);
+int objetos_usuario(vector <int> & objetos, int fasejuego);
+
+// Disparos y turnos
+int disparo(int bala, int enemigo, int & dobleimpacto, bool quejugador, int & vidadisparador, int & vidavictima);
+int bot_turno(const vector <int> & recamara, int indiceactual, int vidabot, int vidajugador);
+int elegir_disparo();
+int mostrar_menu_turno(int fase, const vector <int> & objetos);
+
+// Control del flujo
+int mostrar_estado(string nombrejugador, int vidajugador, int vidabot, int fase, int balasquefaltan);
+int jugarfase(Jugador & jugador, int fase, bool dobleonada);
+int iniciar_partida_nueva();
+int mostrar_menu_principal();
+
+int main() {
+    srand( static_cast <unsigned> ( time(0)) );
+    
+    jugar_juego();
+    
     return 0;
 }
 
-void mostrar_menu(){
-    cout<<"----------Menu----------"<<endl;
-    cout<<"1. Jugar contra CPU"<<endl;
-    cout<<"2. Jugar 1 Vs. 1"<<endl;
-    cout<<"3. Mostrar normes"<<endl;
-    cout<<"4. Sortir"<<endl;
-    cout<<"------------------------"<<endl;  
-}
-
-void procesar_opcio_menu(int opcio){
-    switch (opcio) {
-        case 1: cout<<"Has triat jugar contra la CPU"<<endl; break;
-        case 2: cout<<"Has triat jugar 1 Vs. 1"<<endl; break;
-        case 3: cout<<"Has triat mostrar les normes"<<endl; break;
-        case 4: cout<<"Sortint del programa..."<<endl; break;
-        default: cout<<"Opcio no valida"<<endl; break;
-    }
-}
-
-void triar_opcio_menu(){
-    int opcio = 0;
-    do{
-        mostrar_menu();
-        cout<<"Tria una opcio del menu: ";
-        cin>>opcio;
-        procesar_opcio_menu(opcio);
-    }while(opcio != 4);
-}
